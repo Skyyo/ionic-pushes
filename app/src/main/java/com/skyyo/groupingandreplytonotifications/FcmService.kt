a4180c0fe0013d52e1c439c12b3b9daaa380e4a2
Denis Rudenko <swifttropicbear@gmail.com>
2021-06-07 09:28:55 +0300

    handling notification taps

--- app/src/main/java/com/skyyo/groupingandreplytonotifications/FcmService.kt
+++ app/src/main/java/com/skyyo/groupingandreplytonotifications/FcmService.kt
@@ -1,5 +1,10 @@
 package com.skyyo.groupingandreplytonotifications
 
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -13,6 +18,7 @@ class FcmService : FirebaseMessagingService() {
     //notification channel creation ( required since Android 8 #Oreo)
     val channelId = "your_app_name_channel_id" // doesn't matter much, but rename please
     val channelName = "your_app_name_channel_name" // doesn't matter much, but rename please
+    val activityResultCode = 2313 // doesn't matter much
 
     override fun onCreate() {
         val channel = NotificationChannelCompat.Builder(channelId, IMPORTANCE_HIGH)
@@ -33,22 +39,125 @@ class FcmService : FirebaseMessagingService() {
         val description = remoteMessage.data["description"] ?: return
         val groupName = remoteMessage.data["groupName"] ?: return
         val groupId = remoteMessage.data["groupId"]?.toInt() ?: return
-        showNotification(alertId, title, description, groupName, groupId)
+        val notificationType = remoteMessage.data["type"] ?: return
+        when (notificationType) {
+            "simpleNotification" -> showSimpleNotification(alertId, title, description)
+            "summaryNotification" -> showSummaryNotification(
+                alertId,
+                title,
+                description,
+                groupName,
+                groupId
+            )
+        }
     }
 
-    private fun showNotification(
+    private fun showSimpleNotification(
         notificationId: Int,
         title: String,
         description: String,
-        groupName: String,
-        groupId: Int
     ) {
+        //code that allows us to open activity on notification tap
+        val intentSimpleNotification = Intent(this, MainActivity::class.java).apply {
+
+            // now this is related to the architecture of the app. you can find more info here,
+            // https://developer.android.com/guide/components/activities/tasks-and-back-stack#IntentFlagsForTasks
+            // and choose one that fits you best if needed. Also imporant part here is that activity is declared as singleInstance
+            // inside manifest.xml
+            flags = FLAG_ACTIVITY_SINGLE_TOP
+
+            // you can put key/value pairs here. For example you tap on notification, it takes you
+            // to Product Details Screen, and you want to fetch the info of that product.
+            // That's a situation, when you want productId to be passed from backend into FCM payload, so
+            // it can be used here.
+            putExtra("type", "simpleNotification")
+            putExtra("notificationId", notificationId)
+        }
+        val pendingIntentSimpleNotification = PendingIntent.getActivity(
+            this,
+            activityResultCode,
+            intentSimpleNotification,
+            FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT
+        )
+
         //notification object
         val notification = NotificationCompat.Builder(this, channelId)
             .setContentText(description)
             .setContentTitle(title)
             .setColor(ContextCompat.getColor(this, android.R.color.holo_blue_bright))
             .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntentSimpleNotification)
+            .build()
+
+        NotificationManagerCompat.from(this).notify(notificationId, notification)
+        // this is how we cancel the notification
+        // NotificationManagerCompat.from(this).cancel(notificationId)
+
+    }
+
+    private fun showSummaryNotification(
+        notificationId: Int,
+        title: String,
+        description: String,
+        groupName: String,
+        groupId: Int
+    ) {
+
+        //code that allows us to open activity on notification tap
+        val intentSimpleNotification = Intent(this, MainActivity::class.java).apply {
+
+            // now this is related to the architecture of the app. you can find more info here,
+            // https://developer.android.com/guide/components/activities/tasks-and-back-stack#IntentFlagsForTasks
+            // and choose one that fits you best if needed. Also imporant part here is that activity is declared as singleInstance
+            // inside manifest.xml
+            flags = FLAG_ACTIVITY_SINGLE_TOP
+
+            // you can put key/value pairs here. For example you tap on notification, it takes you
+            // to Product Details Screen, and you want to fetch the info of that product.
+            // That's a situation, when you want productId to be passed from backend into FCM payload, so
+            // it can be used here.
+            putExtra("type", "simpleNotification")
+            putExtra("notificationId", notificationId)
+        }
+        val pendingIntentSimpleNotification = PendingIntent.getActivity(
+            this,
+            activityResultCode,
+            intentSimpleNotification,
+            FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT
+        )
+
+        //code that allows us to open activity on summary notification tap
+        val intentSummaryNotification = Intent(this, MainActivity::class.java).apply {
+
+            // now this is related to the architecture of the app. you can find more info here,
+            // https://developer.android.com/guide/components/activities/tasks-and-back-stack#IntentFlagsForTasks
+            // and choose one that fits you best if needed. Also imporant part here is that activity is declared as singleInstance
+            // inside manifest.xml
+            flags = FLAG_ACTIVITY_SINGLE_TOP
+
+            // you can put key/value pairs here. For example you tap on notification, it takes you
+            // to Product Details Screen, and you want to fetch the info of that product.
+            // That's a situation, when you want productId to be passed from backend into FCM payload, so
+            // it can be used here.
+            putExtra("type", "summaryNotification")
+            putExtra("groupId", groupId)
+        }
+        val pendingIntentSummaryNotification = PendingIntent.getActivity(
+            this,
+            activityResultCode,
+            intentSummaryNotification,
+            FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT
+        )
+
+        //notification object
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentText(description)
+            .setContentTitle(title)
+            .setColor(ContextCompat.getColor(this, android.R.color.holo_blue_bright))
+            .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+            .setContentIntent(pendingIntentSimpleNotification)
+            .setAutoCancel(true)
             .setGroup(groupName)
             .build()
 
@@ -57,7 +166,9 @@ class FcmService : FirebaseMessagingService() {
             .setStyle(NotificationCompat.InboxStyle())
             .setColor(ContextCompat.getColor(this, android.R.color.holo_blue_bright))
             .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+            .setContentIntent(pendingIntentSummaryNotification)
             .setGroup(groupName)
+            .setAutoCancel(true)
             .setGroupSummary(true)
             .build()
 
@@ -65,10 +176,7 @@ class FcmService : FirebaseMessagingService() {
             notify(notificationId, notification)
             notify(groupId, notificationSummary)
         }
-        // this is how we cancel the notification, or notification summary ( group )
-//        NotificationManagerCompat.from(this).cancel(notificationId)
-        // or
-//        NotificationManagerCompat.from(this).cancel(groupId)
+        // this is how we cancel notification summary ( group ) notification
+        // NotificationManagerCompat.from(this).cancel(groupId)
     }
-
 }
--- app/src/main/java/com/skyyo/groupingandreplytonotifications/MainActivity.kt
+++ app/src/main/java/com/skyyo/groupingandreplytonotifications/MainActivity.kt
@@ -1,25 +1,52 @@
 package com.skyyo.groupingandreplytonotifications
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationChannelCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import com.skyyo.groupingandreplytonotifications.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    //this part is view related, doesn't matter
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //this part is view related, doesn't matter
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //we subscribe to the topic here, for testing purposes. You won't need this also
         FirebaseMessaging.getInstance().subscribeToTopic("testTopic")
+
+        // this is how we get the values from our notification, when app starts as a reaction
+        // to user tapping on notification
+        handleNotificationTap(intent.extras)
     }
+
+    //this will be called when app is already running, and user taps on notification
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleNotificationTap(intent?.extras)
+    }
+
+    private fun handleNotificationTap(extras: Bundle?) {
+        when (extras?.get("type")) {
+            "simpleNotification" -> {
+                //user tapped on a specific notification
+                val notificationId = intent.extras?.get("notificationId")
+            }
+            "summaryNotification" -> {
+                //user tapped on a summary(group) notification
+                val notificationId = intent.extras?.get("notificationId")
+                val groupId = intent.extras?.get("groupId")
+            }
+        }
+    }
+
 }
 
 
